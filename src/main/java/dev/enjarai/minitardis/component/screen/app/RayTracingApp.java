2f38c442171425206a9b7a855f6d4836b097699b
Benonardo <75318636+Benonardo@users.noreply.github.com>
2023-11-29 16:16:13 +0100

    Weird normal colors

--- src/main/java/dev/enjarai/minitardis/component/screen/app/RayTracingApp.java
+++ src/main/java/dev/enjarai/minitardis/component/screen/app/RayTracingApp.java
@@ -4,53 +4,72 @@ import com.mojang.serialization.Codec;
 import dev.enjarai.minitardis.MiniTardis;
 import dev.enjarai.minitardis.block.console.ConsoleScreenBlockEntity;
 import dev.enjarai.minitardis.component.TardisControl;
-import eu.pb4.mapcanvas.api.core.CanvasColor;
 import eu.pb4.mapcanvas.api.core.DrawableCanvas;
+import eu.pb4.mapcanvas.api.utils.CanvasUtils;
 import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.util.ClickType;
 import net.minecraft.util.Identifier;
-import org.joml.Vector2f;
-import org.joml.Vector2fc;
-import org.joml.Vector3f;
-import org.joml.Vector3fc;
+import net.minecraft.util.math.ColorHelper;
+import org.joml.*;
+import org.joml.Math;
 
 public class RayTracingApp implements ScreenApp {
 
     public static final Codec<RayTracingApp> CODEC = Codec.unit(RayTracingApp::new);
     public static final Identifier ID = MiniTardis.id("ray_tracing");
 
-    private static final Vector3fc RAY_ORIGIN = new Vector3f(0.0f, 0.0f, -2.0f);
-
     @Override
-    public void draw(TardisControl controls, ConsoleScreenBlockEntity blockEntity, DrawableCanvas canvas) {
-        var coord = new Vector2f();
-        for (var x = 0; x < 128; x++) {
-            for (var y = 0; y < 96; y++) {
-                coord.set((float) x / 128 * 2.0f - 1.0f, (float) y / 96 * 2.0f - ((float) 96 / 128));
-                canvas.set(x, y, perPixel(coord));
+    public AppView getView(TardisControl controls) {
+        return new View();
+    }
+
+    private static class View implements AppView {
+
+        private static final Vector3fc RAY_ORIGIN = new Vector3f(0.0f, 0.0f, -2.0f);
+        private final Vector3f rayDirection = new Vector3f();
+        private final Vector4f pixelColor = new Vector4f();
+        private final Vector3f hitPoint = new Vector3f();
+
+        @Override
+        public void draw(ConsoleScreenBlockEntity blockEntity, DrawableCanvas canvas) {
+            var coord = new Vector2f();
+            for (var x = 0; x < 128; x++) {
+                for (var y = 0; y < 128; y++) {
+                    // math
+                    coord.set((float) x / 128 * 2.0f - 1.0f, (float) y / 128 * 2.0f - 1.0f);
+                    perPixel(coord);
+                    var color = ColorHelper.Argb.getArgb((int) (pixelColor.w * 255), (int) (pixelColor.x * 255), (int) (pixelColor.y * 255), (int) (pixelColor.z * 255));
+                    canvas.set(x, y - (128 - 96) / 2, CanvasUtils.findClosestColorARGB(color));
+                }
             }
         }
-    }
 
-    private CanvasColor perPixel(Vector2fc coord) {
-        var rayDirection = new Vector3f(coord.x(), coord.y(), -1.0f);
-        var radius = 0.5f;
+        private void perPixel(Vector2fc coord) {
+            rayDirection.set(coord.x(), coord.y(), -1.0f);
+            var radius = 0.5f;
 
-        var a = rayDirection.dot(rayDirection);
-        var b = 2.0f * RAY_ORIGIN.dot(rayDirection);
-        var c = RAY_ORIGIN.dot(RAY_ORIGIN) - radius * radius;
+            var a = rayDirection.dot(rayDirection);
+            var b = 2.0f * RAY_ORIGIN.dot(rayDirection);
+            var c = RAY_ORIGIN.dot(RAY_ORIGIN) - radius * radius;
 
-        var discriminant = b * b - 4.0f * a * c;
+            var discriminant = b * b - 4.0f * a * c;
 
-        if (discriminant >= 0.0f)
-            return CanvasColor.PINK_NORMAL;
+            if (discriminant < 0.0f) {
+                pixelColor.set(0.0f, 0.0f, 0.0f, 1.0f);
+                return;
+            }
 
-        return CanvasColor.BLACK_NORMAL;
-    }
+            var closestT = (-b - Math.sqrt(discriminant)) / (2.0f * a);
 
-    @Override
-    public boolean onClick(TardisControl controls, ConsoleScreenBlockEntity blockEntity, ServerPlayerEntity player, ClickType type, int x, int y) {
-        return false;
+            rayDirection.mul(closestT, hitPoint).add(RAY_ORIGIN);
+
+            pixelColor.set(hitPoint.x, hitPoint.y, hitPoint.z, 1.0f);
+        }
+
+        @Override
+        public boolean onClick(ConsoleScreenBlockEntity blockEntity, ServerPlayerEntity player, ClickType type, int x, int y) {
+            return false;
+        }
     }
 
     @Override
